82a17cf45c7e5e704c947594d1231cb3b3f78cdc
cristinavergara1 <cristina.vergara1@udea.edu.co>
2025-11-03 15:36:17 -0500

    Se agrega quita import

--- src/main/java/com/udea/innosistemas/security/GraphQLWebSocketAuthInterceptor.java
+++ src/main/java/com/udea/innosistemas/security/GraphQLWebSocketAuthInterceptor.java
@@ -16,7 +16,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
-import org.springframework.lang.NonNull;
+
 
 
 /**
