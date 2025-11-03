c917ef88eccc07f68cf5cc3a5bae149107bc105c
cristinavergara1 <cristina.vergara1@udea.edu.co>
2025-11-03 15:08:33 -0500

    Se corrige Issues

--- src/main/java/com/udea/innosistemas/security/JwtTokenProvider.java
+++ src/main/java/com/udea/innosistemas/security/JwtTokenProvider.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
