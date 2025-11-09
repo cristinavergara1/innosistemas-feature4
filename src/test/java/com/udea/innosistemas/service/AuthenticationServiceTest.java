9ddfd600ddd8ce830b64c057948b4247676a3793
cristinavergara1 <cristina.vergara1@udea.edu.co>
2025-11-08 19:21:56 -0500

    Se remueve import sin utilizar

--- src/test/java/com/udea/innosistemas/service/AuthenticationServiceTest.java
+++ src/test/java/com/udea/innosistemas/service/AuthenticationServiceTest.java
@@ -19,7 +19,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 
 import java.time.LocalDateTime;
 import java.util.Date;
